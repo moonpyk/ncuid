6437e26f129e3705d201c508478b32557438b469
Clément Bourgeois <moonpyk@gmail.com>
2014-04-21 00:18:33 +0200

    Automatic build generation number.

--- NCuid/Properties/AssemblyInfo.cs
+++ NCuid/Properties/AssemblyInfo.cs
@@ -22,20 +22,6 @@ using System.Runtime.InteropServices;
 // Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
 [assembly: Guid("ca82e0f7-976a-4f3c-bb29-1640af785b97")]
 
-// Les informations de version pour un assembly se composent des quatre valeurs suivantes :
-//
-//      Version principale
-//      Version secondaire 
-//      Numéro de build
-//      Révision
-//
-// Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut 
-// en utilisant '*', comme indiqué ci-dessous :
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("")]
-
 #if DEBUG
   [assembly: InternalsVisibleTo("NCuid.Tests")]
 #endif
